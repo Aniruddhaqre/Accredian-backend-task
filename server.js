71aac27f8773f0bdc63fa8c08d279a6303b31073
Aniruddhaqre <aniruddhamestryqre@gmail.com>
2024-07-20 03:10:39 +0530

    first

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const referralRoute = require("./routes/referralRoute");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+// RESTful endpoints
+app.use("/api/referral" , referralRoute);
+
+app.get("/test" , (req, res) => {
+    res.status(200).send({msg: 'Working Test OK'})
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
