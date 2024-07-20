1e3d725ad5e1a5be5ce9e521d0ce6ec84603b1e4
Aniruddhaqre <aniruddhamestryqre@gmail.com>
2024-07-20 22:55:34 +0530

    second

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const referralRoute = require("./routes/referralRoute");
+const cors = require('cors')
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors())
 
 // RESTful endpoints
 app.use("/api/referral" , referralRoute);
